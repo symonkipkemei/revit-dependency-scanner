7925d88bda43e9e044cdf085010fa3607d7dc663
symonkipkemei <symonkipkemei@gmail.com>
2025-08-31 10:28:26 +0300

    feat: activate github pages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/revit-dependency-scanner',
+  assetPrefix: '/revit-dependency-scanner',
   images: {
     unoptimized: true
   }
